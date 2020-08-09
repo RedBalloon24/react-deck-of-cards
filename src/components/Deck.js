4bcf860542502cfc4895069aa0937d25233780dc
Anonymous <worldtraveler757@gmail.com>
2020-08-09 22:35:51 +0200

    add Deck component

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
+import Card from './Card';
+import "./Deck.css"
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
@@ -19,9 +21,8 @@ class Deck extends Component {
         try {
             let cardUrl = `${API_BASE_URL}/${id}/draw/`
             let cardRes = await Axios.get(cardUrl);
-
             if(!cardRes.data.success) {
-                throw new Error("No card remaining!")
+                throw new Error("No card remaining!");
             }
                
             let card = cardRes.data.cards[0];
@@ -34,19 +35,22 @@ class Deck extends Component {
                         name: `${card.value} of ${card.suit}`
                     }
                 ]
-            
             })); 
         } catch(err) {
             alert(err)
         }
-     
     }
 
     render() {
+        const cards = this.state.drawn.map(c => (
+            <Card key={c.id} name={c.name} image={c.image} />
+        ))
         return (
-            <div>
-                <h1>Card Dealer</h1>
-                <button onClick={this.getCard}>Get Card</button>
+            <div className="Deck">
+                <h1 className="Deck-title">♦ Card Dealer ♦</h1>
+                <h2 className="Deck-title subtitle">♦ Random card demo made with React ♦</h2>
+                <button className="Deck-btn" onClick={this.getCard}>Get Card</button>
+                <div className="Deck-cardarea">{cards}</div>
             </div>
         )
     }
