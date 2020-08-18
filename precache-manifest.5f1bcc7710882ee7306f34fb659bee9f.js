efe5863d074dc3d02dde94f77005d66f30f5c22b
Anonymous <worldtraveler757@gmail.com>
2020-08-18 17:02:54 +0200

    Updates

---
+++ precache-manifest.5f1bcc7710882ee7306f34fb659bee9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3770ee88bb78671fe14c95d65eaeab",
+    "url": "/react-deck-of-cards/index.html"
+  },
+  {
+    "revision": "b696b8ec835294d3251e",
+    "url": "/react-deck-of-cards/static/css/main.83020156.chunk.css"
+  },
+  {
+    "revision": "767322777d89ede57bea",
+    "url": "/react-deck-of-cards/static/js/2.cc270a8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deck-of-cards/static/js/2.cc270a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b696b8ec835294d3251e",
+    "url": "/react-deck-of-cards/static/js/main.091e3b96.chunk.js"
+  },
+  {
+    "revision": "598be0b4b23b2378a184",
+    "url": "/react-deck-of-cards/static/js/runtime-main.d0168346.js"
+  }
+]);
